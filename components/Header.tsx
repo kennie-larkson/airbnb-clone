1c0050b0d027b29d34bdc2beed7e238178e40551
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-11-05 19:01:27 +0100

    calendar and date picker feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,29 @@ import Image from "next/image";
 import * as Icons from "heroicons-react";
 import { useState } from "react";
 
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { isNumberObject } from "util/types";
+
 export default function Header(): JSX.Element {
   const [searchInput, setSearchInput] = useState("");
-  console.log(searchInput);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [guestsCount, setGuestsCount] = useState(1);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges: any) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  console.log("guests", guestsCount);
 
   return (
     <header className="sticky top-0 z-50 bg-white grid grid-cols-3 shadow-md p-5 md:px-10 ">
@@ -47,6 +67,42 @@ export default function Header(): JSX.Element {
           <Icons.UserCircle className="text-gray-500 h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col mx-auto col-span-3 mt-5">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <Icons.Users className="h-5" />
+            <input
+              className="w-12 pl-2 text-lg outline-none text-red-400 "
+              value={guestsCount}
+              onChange={(e) => setGuestsCount(+e.target.value)}
+              type="number"
+              min={1}
+            />
+          </div>
+          {/* <div className="grid grid-cols-2 mx-auto gap-x-60 mt-5 ">
+            <h2 className="text-gray-400 cursor-pointer">Cancel</h2>
+            <h2 className=" text-red-400 cursor-pointer">Search</h2>
+          </div> */}
+          <div className="flex">
+            <button
+              onClick={() => setSearchInput("")}
+              className="flex-grow text-gray-500"
+            >
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
